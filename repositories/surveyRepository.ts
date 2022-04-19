9a03a01562209b6aa34b300206d99641fb782dfd
aleviegas <alexandrev.barrosa@gmail.com>
2022-04-19 13:29:14 -0300

    deleting all files that were duplicaded

--- repositories/surveyRepository.ts
+++
@@ -1,9 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import Survey from "../models/survey";
-
-@EntityRepository(Survey)
-class SurveyRepository extends Repository<Survey> {
-// aqui da para colocar funções personalizadas referente ao banco de dados
-}
-
-export default SurveyRepository
--- repositories/userRepository.ts
+++
@@ -1,9 +0,0 @@
-import { EntityRepository, Repository } from "typeorm";
-import User from "../models/user";
-
-@EntityRepository(User)
-class UserRepository extends Repository<User> {
-
-}
-
-export default UserRepository
